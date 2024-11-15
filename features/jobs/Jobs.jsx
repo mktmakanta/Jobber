5f16cad17de950c2dbc849b5a9187ce679fc533b
mktmakanta <mktmakantamkt@gmail.com>
2024-11-15 23:17:31 +0100

    still working on the filter layout

--- features/jobs/Jobs.jsx
+++ features/jobs/Jobs.jsx
@@ -3,7 +3,7 @@ import JobsFilter from "./JobsFilter";
 
 export default function Filter() {
   return (
-    <main className="flex flex-col min-h-screen">
+    <main className="min-h-screen">
       <Hero />
 
       <JobsFilter />
